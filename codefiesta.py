da8b90818156fdad88688bc3e40a5bffbe982832
yeoyaoyoyo <78098867+yeoyaoyoyo@users.noreply.github.com>
2021-06-27 23:04:07 +0800

    Update codefiesta.py

--- codefiesta.py
+++ codefiesta.py
@@ -16,9 +16,18 @@ def rentorbuy():
 def myClick1():
     USER_INP = simpledialog.askstring(title="Test",
     prompt="What's your Name and Where are you residing currently? Eg. Zong Yao USA California:")
-    userinfo = USER_INP.split()
+    
 
     rentorbuy()
+
+
+def acceptjob():
+    acceptjoblabel = Label(root, text = "Congrats!")
+    acceptjoblabel.pack()
+
+def rejectjob():
+    rejectjoblabel = Label(root, text = "Please try again!")
+    rejectjoblabel.pack()
 
 def findjob():
     USER_INP1 = simpledialog.askstring(title="Test",
@@ -28,17 +37,41 @@ def findjob():
     companyfound = Label(root, text = "You have been matched wtih Microsoft, a leading company in the " + USER_INP1 + " sector. Good luck!")
     companyfound.pack()
 
+    USER_INP1 = simpledialog.askstring(title="Test",
+    prompt="Do you wish to accept this job? Yes or no.")
+
+    if USER_INP1 == "yes":
+        acceptjob()
+    else:
+        rejectjob()
+
 def myClick2():
 
     USER_INP3 = simpledialog.askstring(title="Test",
     prompt="What's your Name and Where are you residing currently? Eg. Zong Yao USA California:")
-    userinfo3 = USER_INP3.split()
+    
 
     findjob()
 
-def schoolinfo():
+
+
+def prischoolinfo():
     USER_INP5 = simpledialog.askstring(title="Test",
     prompt ="You have been enrolled into Greenwood Primary School! Please update on your date of matriculation: (Eg. 20 February 2021")
+
+    finalupdate = Label(root, text="Thank you your update. We look forward to see you on "+ USER_INP5)
+    finalupdate.pack()
+
+def secschoolinfo():
+    USER_INP5 = simpledialog.askstring(title="Test",
+    prompt ="You have been enrolled into Hwa Chong Institution! Please update on your date of matriculation: (Eg. 20 February 2021")
+
+    finalupdate = Label(root, text="Thank you your update. We look forward to see you on "+ USER_INP5)
+    finalupdate.pack()
+
+def terschoolinfo():
+    USER_INP5 = simpledialog.askstring(title="Test",
+    prompt ="You have been enrolled into NUS! Please update on your date of matriculation: (Eg. 20 February 2021")
 
     finalupdate = Label(root, text="Thank you your update. We look forward to see you on "+ USER_INP5)
     finalupdate.pack()
@@ -47,7 +80,14 @@ def findschool():
     USER_INP5 = simpledialog.askstring(title="Test",
     prompt ="For what age are you looking for an education in?:")
 
-    schoolinfo()
+    agetoschool = int(USER_INP5)
+
+    if agetoschool<13:
+        prischoolinfo()
+    elif agetoschool<18:
+        secschoolinfo()
+    else:
+        terschoolinfo()
 
 def myClick3():
     USER_INP4 = simpledialog.askstring(title="Test",
@@ -65,11 +105,6 @@ jobbutton.pack()
 
 educationbutton = Button(root, text = "Education", padx = 110, pady=85, command = myClick3)
 educationbutton.pack()
-
-
-
-
-
 
 
 
